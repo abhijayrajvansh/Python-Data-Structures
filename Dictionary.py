9380f91910ae8e73d1faa15426a0dbe24a74f594
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-04-14 15:13:08 +0530

    DSA With Python

---
+++ Dictionary.py
@@ -0,0 +1,25 @@
+from asyncio.proactor_events import _ProactorBaseWritePipeTransport
+
+
+a = {1:"Abhijay", 2:"Rajvansh", 3:"Bobby"}
+print(a)
+
+print()
+a = {7:"Ronaldo", 10:"Messi"}
+print(a[7])
+
+print(a.get(10))
+print(a)
+a[7] = "Rajvansh"
+print()
+print(a)
+
+print()
+print(a.keys())
+
+print()
+print(a.items())
+
+print()
+a.pop(10)
+print(a)
