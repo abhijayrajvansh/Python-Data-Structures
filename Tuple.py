7bbeada3a4a2f593851a369b071b9be6255e4539
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-04-14 14:49:17 +0530

    DSA With Python

---
+++ Tuple.py
@@ -0,0 +1,24 @@
+a = (1, 2, 3, 4)
+
+print(a)
+print()
+print(a[2])
+
+t1 = (45, 46, 47)
+# t1[0] = 10
+# t1.append(56)
+# not acceptable
+
+a = ()
+print()
+print(dir(a))
+
+print()
+set1 = (45, 67, 78, 98, 34, 23, 1, 1, 1, 1, 1, "Abhijay", "Rajvansh")
+print(set1.index("Abhijay"))
+
+print()
+print(len(set1))
+
+print()
+print(set1.count(1))
